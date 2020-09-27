e970aea93f98806cf69c9d175ba6f70c594c2f87
ggianini <50370164+ggianini@users.noreply.github.com>
2020-09-27 17:03:54 -0300

    Update readiness.php

--- readiness.php
+++ readiness.php
@@ -4,9 +4,6 @@ $filename = '/tmp/readiness';
 if (file_exists($filename)) {
     header("HTTP/1.1 500 Internal Server Error");
 } else {
-    echo "Estou pronto!";
-    echo "";
-    echo "Essa mensagem significa que a aplicação está pronta para ser balanceada.";
-    echo "Caso essa URL devolva um erro 500, significa que o readiness falhou. Nesse caso o OpenShift irá retirar o pod do balanceador";
+    echo "Estou pronto! \n Essa mensagem significa que a aplicação está pronta para ser inserida no balanceador. \n Caso essa URL devolva um erro 500, significa que o readiness falhou. Nesse caso o OpenShift irá remover a aplicação do balanceamento";
 }
 ?>
