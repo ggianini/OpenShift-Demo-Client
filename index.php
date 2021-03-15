393e659933a58b8255a64a94fd7dd074f22cf5a6
ggianini <50370164+ggianini@users.noreply.github.com>
2021-03-14 22:41:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,11 @@ echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
 
-$conn = new pg_connect("postgresql", "user1", "admin", "sampledb");
+$conn = new mysqli("mysql", "redhat", "redhat", "sampledb");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $result = $conn->query("SELECT nome FROM cidade");
-
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<h3>" . $row["nome"] . "</h3>";
