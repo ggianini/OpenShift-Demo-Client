f67cc6a747a583c4030d75c7668578aa06e110a3
Gustavo Gianini <50370164+ggianini@users.noreply.github.com>
2021-09-02 11:38:14 -0300

    Update liveness.php

--- liveness.php
+++ liveness.php
@@ -1,12 +1,10 @@
 <?php
-$filename = '/tmp/liveness';
+$filename = '/tmp/falha';
 
 if (file_exists($filename)) {
     header("HTTP/1.1 500 Internal Server Error");
 } else {
-        echo "Estou vivo!";
-        echo "<br><hr>";
-        echo "Essa mensagem significa que a aplicação está saudável.";
+        echo "Não encontrei falhas!";
         echo "<br><hr>";
         echo "Caso essa URL devolva um erro 500, significa que o liveness falhou. Nesse caso o OpenShift irá deletar o pod";
 }
