9e9f20579eab36f3373a2b610de50e0b5030e25b
Gustavo Gianini <50370164+ggianini@users.noreply.github.com>
2021-09-02 11:40:23 -0300

    Update readiness.php

--- readiness.php
+++ readiness.php
@@ -1,13 +1,11 @@
 <?php
-$filename = '/tmp/readiness';
+$filename = '/tmp/falha-dependencia';
 
 if (file_exists($filename)) {
     header("HTTP/1.1 500 Internal Server Error");
 } else {
-        echo "Estou pronto!";
+        echo "Todas as dependências ok!";
         echo "<br><hr>";
-        echo "Essa mensagem significa que a aplicação está pronta para ser inserida no balanceamento.";
-        echo "<br><hr>";
-        echo "Caso essa URL devolva um erro 500, significa que o liveness falhou. Nesse caso o OpenShift irá remover a aplicação do balanceador.";
+        echo "Caso essa URL devolva um erro 500, o OpenShift irá remover a aplicação do balanceador.";
 }
 ?>
